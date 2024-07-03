96645ce7e703a14b770fe225ea5de35088a36f53
kishan-ck <kishansutariya.coderkube@gmail.com>
2024-07-03 10:08:28 +0530

    electron implemented

--- public/electron.js
+++ public/electron.js
@@ -1,36 +1,42 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
-const url = require("url");
-function createWindow() {
-  const startUrl =
-    process.env.ELECTRON_START_URL ||
-    url.format({
-      pathname: path.join(__dirname, "../index.html"),
-      protocol: "file:",
-      slashes: true,
+
+(async () => {
+  const isDev = (await import("electron-is-dev")).default;
+
+  function createWindow() {
+    const mainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, "../public/preload.js"),
+        nodeIntegration: true,
+        contextIsolation: false, // Ensure context isolation is disabled if needed
+      },
     });
-  const win = new BrowserWindow({
-    width: 800,
-    height: 800,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
-  win.loadURL(startUrl);
+
+    mainWindow.loadURL(
+      isDev
+        ? "http://localhost:3000"
+        : `file://${path.join(__dirname, "../build/index.html")}`
+    );
+
+    if (isDev) {
+      mainWindow.webContents.openDevTools();
+    }
+  }
+
+  app.on("ready", createWindow);
+
   app.on("window-all-closed", () => {
     if (process.platform !== "darwin") {
       app.quit();
     }
   });
-}
-app.whenReady().then(createWindow);
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
-app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+})();
